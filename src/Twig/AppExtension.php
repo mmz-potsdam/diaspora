6ea0baad4667775fdf13a77bdefa244accfe6e6e
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2025-08-20 16:28:15 +0200

    Add markdown_to_html filter for person.description

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -4,20 +4,23 @@
 
 namespace App\Twig;
 
+use League\CommonMark\ConverterInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 
 class AppExtension extends AbstractExtension
 {
-    protected $translator;
+    protected TranslatorInterface $translator;
+    protected ConverterInterface $markdownConverter;
 
     /**
      * ingest services
      */
-    public function __construct(TranslatorInterface $translator)
+    public function __construct(TranslatorInterface $translator, ConverterInterface $markdownConverter)
     {
         $this->translator = $translator;
+        $this->markdownConverter = $markdownConverter;
     }
 
     /**
@@ -26,9 +29,21 @@ class AppExtension extends AbstractExtension
     public function getFilters(): array
     {
         return [
+            // minimal markdown for person bios
+            new TwigFilter('markdown_to_html', [$this, 'convertMarkdownToHtml'], ['is_safe' => ['all']]),
+
             // site specific
             new TwigFilter('extract_genres', 'App\\Controller\\KeywordController::extractGenres'),
             new TwigFilter('extract_topics', 'App\\Controller\\KeywordController::extractTopics'),
         ];
+    }
+
+    public function convertMarkdownToHtml(?string $markdown): string
+    {
+        if (is_null($markdown)) {
+            return '';
+        }
+
+        return $this->markdownConverter->convert($markdown);
     }
 }
